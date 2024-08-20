a6864164dbd073b0aeacd00eca6e04540872f8fd
fzl51 <365633133@qq.com>
2024-08-20 22:43:54 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-const {render} = require("./server/entry-server");
-const ssrMainFest = require('./server/ssr-manifest.json');
+const webPath = './' //设置server根client文件所在路径
+const {render} = require(`${webPath}server/entry-server`);
 const express = require('express');
 const app = express();
 const fs = require('fs');
 const path = require('path');
-const templatePath = path.resolve(__dirname, './server/index.html');// 读取模板 HTML 文件
+const templatePath = path.resolve(__dirname, `${webPath}server/index.html`);// 读取模板 HTML 文件
+const ssrMainFest = require(`${webPath}server/ssr-manifest.json`);
 let templateHtml = fs.readFileSync(templatePath, 'utf-8');
+
+//获取路由
 const routes = __uniRoutes.reduce((acc, item) => {
     acc[item.path] = item;
     if (item['alias']) {
@@ -15,7 +18,6 @@ const routes = __uniRoutes.reduce((acc, item) => {
 }, {});
 
 app.use(async (req, res, next) => {
-
     if (!routes[req.path]) return next()
     try {
         console.log('req=>', req.path)
@@ -25,12 +27,23 @@ app.use(async (req, res, next) => {
         let finalHtml = templateHtml
             .replace('<!--preload-links-->', preloadLinks)
             .replace('<!--app-context-->', appContext)
-            .replace('<!--app-html-->', `${appHtml}`);
+            .replace('<!--app-html-->', `${appHtml}`)
+            .replace(/(<head[^>]*>)(?!.*<head[^>]*>)/i, `$1\n${headMeta}\n`);
+
         // 设置标题
-        finalHtml = finalHtml.replace(/<title>(.*?)<\/title>/, `<title>${title}</title>`);
-        finalHtml = finalHtml.replace(/鼠标右键查看源码 如果源码存在此文字则成功/, `
-        鼠标右键查看源码 如果源码存在此文字则成功
-        `);
+        finalHtml = finalHtml.replace(/<title>(.*?)<\/title>/g, `<title>${title}</title>`);
+
+
+        // finalHtml = finalHtml.replaceAll('uni-view', `div`);
+        // finalHtml = finalHtml.replaceAll('uni-text', `span`);
+        // finalHtml = finalHtml.replaceAll('uni-app', `div`);
+        // finalHtml = finalHtml.replaceAll('uni-page-wrapper', `div`);
+        // finalHtml = finalHtml.replaceAll('uni-page-body', `div`);
+        // finalHtml = finalHtml.replaceAll('uni-page', `div`);
+
+
+        //替换注释
+        finalHtml = finalHtml.replace(/<!--[\s\S]*?-->|\/\*[\s\S]*?\*\//g, '');
         // 返回最终的 HTML
         return res.send(finalHtml);
     } catch (err) {
@@ -39,7 +52,7 @@ app.use(async (req, res, next) => {
     }
 
 })
-app.use(express.static('client'));
+app.use(express.static(`${webPath}client`));
 process.on('uncaughtException', (err) => {
     console.error('未捕获的异常:', err);
 });
